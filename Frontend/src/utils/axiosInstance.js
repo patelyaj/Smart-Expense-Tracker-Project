a2e331e5638066adc7c1c87559f076c7a7075bbe
patelyaj <yaj@huptechweb.com>
2026-03-09 13:23:04 +0530

    provided backend deployed url to frontend

--- Frontend/src/utils/axiosInstance.js
+++ Frontend/src/utils/axiosInstance.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const BACKEND_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/expensetracker';
+
 const api = axios.create({
-  baseURL: 'http://localhost:5000/expensetracker', 
+  baseURL: BACKEND_URL, 
   withCredentials: true,
 });
 
