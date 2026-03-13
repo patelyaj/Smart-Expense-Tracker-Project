897733f8423f7791f37435718b3ab6d4030d0126
patelyaj <yaj@huptechweb.com>
2026-03-13 16:51:39 +0530

    Add budget routes for CRUD operations

---
+++ Backend/routes/budgetRoutes.js
@@ -0,0 +1,25 @@
+import express from "express";
+import {
+  fetchBudget,
+  createBudget,
+  updateBudget,
+  deleteBudget,
+  fetchBudgetProgress,
+  getBudgetDetails
+} from "../controller/budgetController.js";
+
+import { verifyToken } from "../middlewares/authMiddleware.js";
+
+const router = express.Router();
+
+router.get("/progress/:userId", verifyToken, fetchBudgetProgress);
+router.get("/details/:budgetId", verifyToken, getBudgetDetails);
+router.get("/:userId", verifyToken, fetchBudget);
+
+router.post("/create", verifyToken, createBudget);
+
+router.patch("/:budgetId", verifyToken, updateBudget);
+
+router.delete("/:budgetId", verifyToken, deleteBudget);
+
+export default router;
