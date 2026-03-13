517f77b263670e6134150c6dacd49638e68a0f4d
patelyaj <yaj@huptechweb.com>
2026-03-13 15:48:45 +0530

    Refactor route imports for transaction and category

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -1,15 +1,15 @@
 import express from "express";
 import authRoutes from './authRoutes.js';
-import transactionRouter from './transactionRoutes.js';
-import categoryRouter from './categoryRoutes.js';
-import budgetRouter from './budgetRoutes.js';
+import transactionRoutes from './transactionRoutes.js';
+import categoryRoutes from './categoryRoutes.js';
+import budgetRouter from './budgetRoutes.js'
 
 
 const router = express.Router();
 
 router.use('/users',authRoutes);
-router.use('/transactions',transactionRouter);
-router.use('/categories',categoryRouter);
+router.use('/transactions',transactionRoutes);
+router.use('/categories',categoryRoutes);
 router.use('/budget',budgetRouter);
 
 export default router;
