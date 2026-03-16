6a21999bae3211ff136bc4df0c80c1cf74e84fcf
patelyaj <yaj@huptechweb.com>
2026-03-16 13:24:14 +0530

    axiosInstance url fix

--- Frontend/src/utils/axiosInstance.js
+++ Frontend/src/utils/axiosInstance.js
@@ -1,32 +1,66 @@
 import axios from 'axios';
 
-//backed url = VITE_API_URL
-const BACKEND_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+// If running locally → use local backend
+// If deployed → use Vercel rewrite
+const BACKEND_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5000/api"
+    : "/api";
 
 const api = axios.create({
-  baseURL: BACKEND_URL, 
+  baseURL: BACKEND_URL,
   withCredentials: true,
 });
 
-// Response Interceptor: This runs every time the backend sends a response
+// Response Interceptor
 api.interceptors.response.use(
   (response) => {
-    // If the request is successful (200 OK), just return the data
     return response;
-  },  
+  },
   (error) => {
-    // If the server returns 401 (Unauthorized), it means the cookie is expired or missing
     if (error.response && error.response.status === 401) {
       console.log("Session expired. Clearing data...");
       
-      localStorage.removeItem('userInfo');
-      
-      // This ensures the Redux state is also cleared and the user is kicked out
-      window.location.href = '/login'; 
+      localStorage.removeItem("userInfo");
+
+      window.location.href = "/login";
     }
-    
+
     return Promise.reject(error);
   }
 );
 
 export default api;
+
+// import axios from 'axios';
+
+// //backed url = VITE_API_URL
+// const BACKEND_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+
+// const api = axios.create({
+//   baseURL: BACKEND_URL, 
+//   withCredentials: true,
+// });
+
+// // Response Interceptor: This runs every time the backend sends a response
+// api.interceptors.response.use(
+//   (response) => {
+//     // If the request is successful (200 OK), just return the data
+//     return response;
+//   },  
+//   (error) => {
+//     // If the server returns 401 (Unauthorized), it means the cookie is expired or missing
+//     if (error.response && error.response.status === 401) {
+//       console.log("Session expired. Clearing data...");
+      
+//       localStorage.removeItem('userInfo');
+      
+//       // This ensures the Redux state is also cleared and the user is kicked out
+//       window.location.href = '/login'; 
+//     }
+    
+//     return Promise.reject(error);
+//   }
+// );
+
+// export default api;
