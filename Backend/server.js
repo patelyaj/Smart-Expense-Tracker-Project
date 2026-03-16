d457d0f52e2f57a8fcd96824573d0e892c5cca92
patelyaj <yaj@huptechweb.com>
2026-03-16 11:36:57 +0530

    cors fixed

--- Backend/server.js
+++ Backend/server.js
@@ -29,7 +29,7 @@ const allowedOrigins = [
 const corsOptions = {
   origin: allowedOrigins,
   credentials: true,
-  methods: ["GET","POST","PUT","DELETE","OPTIONS"],
+  methods: ["GET","POST","PUT","DELETE","PATCH","OPTIONS"],
   allowedHeaders: ["Content-Type","Authorization"]
 };
 
